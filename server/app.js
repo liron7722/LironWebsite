c7f273af64027d5c419f28bcc9522a7703787eb3
liron7722 <liron7722@gmail.com>
2020-08-26 02:29:44 +0300

    small improvment

--- server/app.js
+++ server/app.js
@@ -1,15 +1,26 @@
-const serverType = 'Development';
-const local_IP = '127.0.0.1';
-const port = 3000;
+const serverType = process.env.NODE_ENV;
+const local_IP = 'localhost';
+const port = process.env.NODE_PORT;
+const httpPort = 8080;
+const httpsPort = 8443;
 
 const ip = require("ip");
 const fs = require('fs');
+const http = require('http');
+const https = require('https');
 const path = require('path');
 const express = require('express');
 const publicIp = require('public-ip');
-const app = express();
 
 const filesPath = path.resolve(__dirname, '..');
+
+/*
+let privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
+let certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+let credentials = {key: privateKey, cert: certificate};
+*/
+const app = express();
+
 
 app.set("view engine", "ejs");
 app.set('views', filesPath + path.sep + 'templates');  // ejs files path
@@ -19,45 +30,10 @@ app.use('/', (req, res) => {
   res.render("home");
 });
 
-
-  //res.status(200).type('html');
-  /*
-  fs.readFile('../templates/home.html', function(error, data) {
-    if (error) {
-	res.writeHead(404);
-	res.write('Error: File Not Found');
-      } else {
-	res.writeHead(200);
-	res.write(data);
-      }
-    console.log('Someone enter your website');
-      res.end();
-    });
-});*/
-/*
-app.use('/a', (req, res) => {
-  var method = req.method;
-  var url = req.url;
-  var agent = req.get('User-Agent');
-
-  res.status(200).type('html');
-  res.write('Hello World');
-  res.end();
-});
-
-
-app.use('/about', (req, res) => {
-  res.send('This is the about page.');
-});
-
-app.use('/login', (req, res) => {
-  res.send('This is the login page.');
-});
-*/
-
 app.use(/*default*/ (req, res) => {
   res.status(404).send('Not found!');
 });
+
 
 app.listen(port, () => {
   console.log(`Server type is: ${serverType}`);
@@ -67,3 +43,23 @@ app.listen(port, () => {
     console.log(`Server public address is http://${await publicIp.v4()}:${port}`);
   })();
 });
+
+/**/
+/*
+function startLog() {
+  console.log(`Server type is: ${serverType}`);
+  console.log(`Server internal ip address ${ip.address()}`);
+  console.log(`Server local address is http://${local_IP}:${httpsPort}`);
+  (async () => {
+    console.log(`Server public address is http://${await publicIp.v4()}:${httpsPort}`);
+  })();
+}
+
+
+let httpServer = http.createServer(app);
+let httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(httpPort);
+httpsServer.listen(httpsPort);
+startLog();
+*/
